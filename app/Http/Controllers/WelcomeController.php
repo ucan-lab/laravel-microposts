a384a2bbd2b0f9f78001bbc9c77c068086363af1
ゆうきゃん <35098175+ucan-lab@users.noreply.github.com>
2018-05-27 01:00:37 +0900

    timeline

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -17,7 +17,7 @@ class WelcomeController extends Controller
         $data = [];
         if (\Auth::check()) {
             $user = \Auth::user();
-            $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(10);
+            $microposts = $user->feed_microposts()->orderBy('created_at', 'desc')->paginate(10);
 
             $data = [
                 'user' => $user,
